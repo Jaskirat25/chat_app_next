2979015a117a57efa928362828f92aa87a11363d
jaskirat <jaskiratsingh251103@gmail.com>
2026-05-06 00:05:26 +0530

    build error resolve

--- src/app/api/username-check/route.ts
+++ src/app/api/username-check/route.ts
@@ -1,15 +1,15 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
+import prisma from "../../../../lib/prisma";
 
-export async function GET(req:Request){
-    const {searchParams}=new URL(req.url);
-    const username=searchParams.get("username");
-    if(!username)return NextResponse.json({available:"Not Available"});
-    const user=await prisma.user.findFirst({
-        where:{
-            username:username,
-        }
-    });
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const username = searchParams.get("username");
+  if (!username) return NextResponse.json({ available: "Not Available" });
+  const user = await prisma.user.findFirst({
+    where: {
+      username: username,
+    },
+  });
 
-    return NextResponse.json({isAvailable:user?false:true});
+  return NextResponse.json({ isAvailable: user ? false : true });
 }
