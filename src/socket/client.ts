a0c8f26f53e719266d095a61b93d8eddd6f34074
jaskirat <jaskiratsingh251103@gmail.com>
2026-05-06 14:49:57 +0530

    socket edit

--- src/socket/client.ts
+++ src/socket/client.ts
@@ -1,10 +1,14 @@
 "use client";
+
 import Cookies from "js-cookie";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
-  auth:{
-    token:Cookies.get("auth-token")
-  },
-  autoConnect: false,
-});
+export const socket = io(
+  "https://chat-app-server-ah27.onrender.com",
+  {
+    auth: {
+      token: Cookies.get("auth-token"),
+    },
+    autoConnect: false,
+  }
+);
